4c31a0717ae6a9a440f805ac0ff55e46ccd593a9
schiplun <schiplun@LIN51013721.corp.capgemini.com>
2021-07-07 15:12:56 +0530

    employee class added

--- demo/src/main/java/demo/demoClass.java
+++ demo/src/main/java/demo/demoClass.java
@@ -4,7 +4,11 @@ public class demoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("hello");
+		System.out.println("welcome");
+		System.out.println("to my");
+		System.out.println("my world");
+		
 	}
 
 }
