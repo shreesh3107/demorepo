5b7051f9ccd3f25a036791f68412c2b502c4a55a
shreesh31 <shreesh.chip@outlook.com>
2021-07-08 13:16:44 +0530

    employye name commit

--- demo/src/main/java/demo/Employee.java
+++ demo/src/main/java/demo/Employee.java
@@ -1,6 +1,10 @@
 package demo;
 
 public class Employee {
+	
+	public void name() {
+		System.out.println("this is me");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
